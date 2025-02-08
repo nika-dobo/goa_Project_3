86219e52ef7b5c00b8949038fb329262200ce9c0
nika-dobo <nikadobo20@gmail.com>
2025-02-08 21:52:13 +0400

    sd

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import mymodule as my
 
 
-if my.card_ID1 == card_ID and my.password1 == password and my.acc_name1 == acc_name and my.ID1 == ID and my.name1 == name and my.surname1 == surname and my.age1 == age and my.email1 == email and my.mobile_number1 == mobile_number and my.main_address1 == main_address and my.formal_address1 == formal_address:
-    print(12121)
+
 
